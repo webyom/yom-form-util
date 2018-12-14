632e4ce939d66ecced9d6c0abd77a3b2dcf3b321
webyom <webyom@gmail.com>
2018-12-14 20:41:51 +0800

    1.0.1

--- dist/yom-form-util.js
+++ dist/yom-form-util.js
@@ -940,13 +940,26 @@ module.exports = function(item, maxLen) {
 
 var $ = __webpack_require__(0);
 
-function _getByteLength(str) {
-	return str.replace(/[^\x00-\xff]/g, 'xx').length;
+function getByteLength(str) {
+  var res = 0;
+  for(var i = 0, l = str.length; i < l; i++) {
+    var charCode = str.charCodeAt(i);
+    if(charCode <= 0x007f) {
+      res++;
+    } else if(charCode <= 0x07ff) {
+      res += 2;
+    } else if(charCode <= 0xffff) {
+      res += 3;
+    } else {
+      res += 4;
+    }
+  }
+  return res;
 }
 
 module.exports = function(item, maxLen) {
 	item = $(item)[0];
-	var inputLen = _getByteLength(item.value);
+	var inputLen = getByteLength(item.value);
 	var passed = inputLen <= maxLen;
 	return {
 		passed: passed,
