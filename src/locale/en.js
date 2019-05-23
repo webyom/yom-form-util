2a9cc721798fbe1c16c58f0e96a997af32dce867
webyom <webyom@gmail.com>
2019-05-23 13:29:20 +0800

    1.4.2

--- src/locale/en.js
+++ src/locale/en.js
@@ -1,24 +1,24 @@
 var YomFormUtil = require('yom-form-util');
 
 YomFormUtil.setCommonMsg({
-  "datetime": "Invalid datetime format.",
-  "domain": "Invalid domain name.",
-  "domain-list": "Invalid domain name.",
+  "datetime": "Invalid datetime format",
+  "domain": "Invalid domain name",
+  "domain-list": "Invalid domain name",
   "email": "Invalid email address",
   "email-list": "Invalid email address",
-  "integer": "It must be an integer.",
-  "integer-range": "The integer value must be between {{0}} to {{1}}",
+  "integer": "Must enter integer",
+  "integer-range": "The integer value must be between {{0}} and {{1}}",
   "mandatory": "Input required",
-  "max-byte-length": "Input length exceeded. Please cut down {{1}} character(s).",
-  "max-length": "Input length exceeded. Please cut down {{1}} character(s).",
+  "max-byte-length": "Input length exceeded, please cut down {{1}} character",
+  "max-length": "Input length exceeded, please cut down {{1}} character",
   "mobile": "Invalid mobile number",
   "name": "Invalid name",
-  "number": "It must be a number.",
-  "number-digits": "{{0}} decimal reserved",
-  "number-range": "The number value must be between {{0}} to {{1}}",
+  "number": "Must enter number",
+  "number-digits": "Reserved to {{0}} decimal places",
+  "number-range": "The number value must be between {{0}} and {{1}}",
   "password": "The length of password must be between 6 and 16. The password must contain upper-case and lower-case alphabetic characters as well as numeric characters. It must not contain blank spaces.",
-  "set": "Duplicate value is not allowed. The max length of a value is 80, and values cannot contain \"||\".",
-  "trim": "Spaces at the beginning and end are not allowed.",
-  "url": "Invalid URL.",
-  "word-upper-case": "Only upper-case letter, digit or underscore is allowed."
+  "set": "Duplicate value is not allowed. The max length of a value is 80, and values cannot contain \"||\"",
+  "trim": "Spaces at the beginning and end are not allowed",
+  "url": "Invalid URL",
+  "word-upper-case": "Only upper-case letter, digit or underscore is allowed"
 });
